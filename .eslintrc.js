322a76dbc5375ee1702f0f389b3d270de647ede0
Tobias Trinks <tobias.trinks@code.berlin>
2018-02-02 14:17:15 +0100

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": [
+        "standard",
+        "plugin:react/recommended"
+    ],
+    "rules": {
+        "indent": ["error", 4],
+        "semi": ["error", "always"],
+        "no-trailing-spaces": ["error", { "skipBlankLines": true }]
+    }
+};
